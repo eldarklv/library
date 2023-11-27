8691747cc0a1d5b69addc71fc04548b403535f67
Ildar Kuluev <ildar.l.kuluev63@gmail.com>
2023-11-28 01:09:48 +0300

    add books router

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 require("dotenv").config();
-const Book = require("./models/Book");
-const { library } = require("./database/collections");
 const userRouter = require("./routes/user");
+const booksRouter = require("./routes/books");
 
 const port = process.env.PORT || 3000;
 
@@ -10,77 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use("/api/user", userRouter);
-
-app.get("/api/books", (req, res) => {
-  const books = library;
-  res.status(200);
-  res.json(books);
-});
-
-app.get("/api/books/:id", (req, res) => {
-  const books = library;
-  const { id } = req.params;
-  const idx = books.findIndex((item) => item.id === id);
-
-  if (idx !== -1) {
-    res.json(books[idx]);
-  } else {
-    res.status(404);
-    res.json("404 | Книга не найдена");
-  }
-});
-
-app.post("/api/books", (req, res) => {
-  const books = library;
-
-  const newBook = new Book(
-    req.body.title,
-    req.body.description,
-    req.body.authors,
-    req.body.favorite,
-    req.body.fileCover,
-    req.body.fileName
-  );
-
-  books.push(newBook);
-  res.status(201);
-  res.json(newBook);
-});
-
-app.put("/api/books/:id", (req, res) => {
-  const books = library;
-
-  const { id } = req.params;
-  const idx = books.findIndex((item) => item.id === id);
-
-  // редактирование найденной книги с помощью rest оператора
-  if (idx !== -1) {
-    books[idx] = {
-      ...books[idx],
-      ...req.body,
-    };
-
-    res.json(books[idx]);
-  } else {
-    res.status(404);
-    res.json("404 | Книга не найдена");
-  }
-});
-
-app.delete("/api/books/:id", (req, res) => {
-  const books = library;
-
-  const { id } = req.params;
-  const idx = books.findIndex((item) => item.id === id);
-
-  if (id !== -1) {
-    books.splice(idx, 1);
-    res.json("ok");
-  } else {
-    res.status(404);
-    res.json("404 | Книга не найдена");
-  }
-});
+app.use("/api/books", booksRouter);
 
 app.listen(port, () => {
   console.log(`Приложение запущено на порту ${port}`);
--- routes/books.js
+++ routes/books.js
@@ -1,5 +1,78 @@
 const express = require("express");
+const { library } = require("../database/collections");
+const Book = require("../models/Book");
 
 const router = express.Router();
 
-router.
+router.get("/", (req, res) => {
+  const books = library;
+  res.status(200);
+  res.json(books);
+});
+
+router.get("/:id", (req, res) => {
+  const books = library;
+  const { id } = req.params;
+  const idx = books.findIndex((item) => item.id === id);
+
+  if (idx !== -1) {
+    res.json(books[idx]);
+  } else {
+    res.status(404);
+    res.json("404 | Книга не найдена");
+  }
+});
+
+router.post("/", (req, res) => {
+  const books = library;
+
+  const newBook = new Book(
+    req.body.title,
+    req.body.description,
+    req.body.authors,
+    req.body.favorite,
+    req.body.fileCover,
+    req.body.fileName
+  );
+
+  books.push(newBook);
+  res.status(201);
+  res.json(newBook);
+});
+
+router.put("/:id", (req, res) => {
+  const books = library;
+
+  const { id } = req.params;
+  const idx = books.findIndex((item) => item.id === id);
+
+  // редактирование найденной книги с помощью rest оператора
+  if (idx !== -1) {
+    books[idx] = {
+      ...books[idx],
+      ...req.body,
+    };
+
+    res.json(books[idx]);
+  } else {
+    res.status(404);
+    res.json("404 | Книга не найдена");
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  const books = library;
+
+  const { id } = req.params;
+  const idx = books.findIndex((item) => item.id === id);
+
+  if (id !== -1) {
+    books.splice(idx, 1);
+    res.json("ok");
+  } else {
+    res.status(404);
+    res.json("404 | Книга не найдена");
+  }
+});
+
+module.exports = router;
