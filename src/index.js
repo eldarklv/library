bf04144ff8ef5aa8263b2e9e82bc2e2be72cee17
Ildar Kuluev <ildar.l.kuluev63@gmail.com>
2023-12-19 02:30:10 +0300

    add mongo db integration

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 require("dotenv").config();
 const userRouter = require("./routes/user");
 const booksRouter = require("./routes/books");
+const mongoose = require("mongoose");
 
 const port = process.env.PORT || 3000;
+const mongo_url = process.env.MONGO_URL
 
 const app = express();
 
@@ -12,6 +14,7 @@ app.use("/api/user", userRouter);
 app.use("/api/books", booksRouter);
 app.use(express.static("database/fileBooks"));
 
+mongoose.connect(mongo_url).then(() => console.log("Mongo connected!"));
 app.listen(port, () => {
   console.log(`Приложение запущено на порту ${port}`);
 });
