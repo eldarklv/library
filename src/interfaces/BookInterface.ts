09887788b4fdc5a8bad19d82d9a45fddf90b6106
Ildar Kuluev <ildar.l.kuluev63@gmail.com>
2024-02-09 01:55:30 +0300

    do interface and absract class

---
+++ src/interfaces/BookInterface.ts
@@ -0,0 +1,15 @@
+export interface Book {
+  title: string,
+
+  description: string,
+
+  authors: string,
+
+  favorite: string,
+
+  fileCover: string,
+
+  fileName: string,
+
+  fileBook: string
+}
