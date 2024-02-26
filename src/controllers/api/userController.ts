5fe93a0c918af3453b177846dbb1a1015e8861b0
Ildar Kuluev <ildar.l.kuluev63@gmail.com>
2024-02-27 00:48:58 +0300

    refactor api user controller

---
+++ src/controllers/api/userController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+
+class userController  {
+  login (req: Request, res: Response) {
+    res.status(201);
+    res.json({ id: 1, mail: "test@mail.ru" });
+  }
+}
+
+module.exports = userController
